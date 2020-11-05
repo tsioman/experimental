38937908809f2d1827b7aa7dbde873fcdcf58d4d
Alexander Tsioma <a.tsioma84@gmail.com>
2020-11-05 14:05:20 +0100

    add: webpack + react/preact

---
+++ preact/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    alias: {      
+      components: path.resolve(__dirname, "src/components"),
+      "@": path.resolve(__dirname, "src"),
+      css: path.resolve(__dirname, "css"),
+    },
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        loader: require.resolve("babel-loader"),
+        exclude: /node_modules/,
+      }     
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+    },
+  },
+};
